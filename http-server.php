558bb1c1ab1dc61016ad91484ff034bc8b65a6c3
Filipe Leuch Bonfim <filipe1309@gmail.com>
2021-08-26 20:13:59 -0300

    feat: request object

--- http-server.php
+++ http-server.php
@@ -1,11 +1,14 @@
 <?php
 
-use Swoole\Http\Server;
+use Swoole\Http\{Request, Response, Server};
 
 $server = new Server('0.0.0.0', 8080);
 
-$server->on('request', function ($request, $response) {
-    $response->end("<h1>Hello Swoole!</h1>");
+// http://localhost:8080/
+$server->on('request', function (Request $request, Response $response) {
+    // var_dump($request->get);
+    $response->header('Content-Type', 'text/html; charset=utf-8');
+    $response->end(print_r($request->header, true));
 });
 
 $server->start();
